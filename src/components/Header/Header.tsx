a6cf1f5777465f093a06a77678ef66aab9751055
anhntph14690 <anhntph14690@fpt.edu.vn>
2022-10-14 22:11:48 +0700

    Edit <a/> => <Link/> Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,14 +9,14 @@ const Header = () => {
           {/* navbar */}
           <nav className="flex justify-between bg-[#00263a] text-white w-screen">
             <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-              <a className="w-20" href="#">
+              <Link className="w-20" to="#">
                 {/* <img class="h-9" src="logo.png" alt="logo"> */}
                 <img
                   src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1664895627/poly_wareh_j06pfe.png"
                   alt=""
                   width="90px"
                 />
-              </a>
+              </Link>
               {/* Nav Links */}
               <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
                 <li>
@@ -144,7 +144,7 @@ const Header = () => {
               </ul>
               {/* Header Icons */}
               <div className="hidden xl:flex items-center space-x-5 items-center">
-                <a className="flex items-center hover:text-gray-200" href="#">
+                <Link className="flex items-center hover:text-gray-200" to="#">
                   <span className="inline-flex justify-center items-center ml-4">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9">
@@ -155,14 +155,14 @@ const Header = () => {
                     <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
                     <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500" />
                   </span>
-                </a>
+                </Link>
 
                 <div className="group inline-block">
                   <ul className="outline-none focus:outline-none px-3 py-1 rounded-sm flex items-center min-w-32">
                     <span className="pr-1 font-semibold flex-1">
-                      <a
+                      <Link
                         className="flex items-center hover:text-gray-200"
-                        href="#"
+                        to="#"
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -178,7 +178,7 @@ const Header = () => {
                             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                           />
                         </svg>
-                      </a>
+                      </Link>
                     </span>
                   </ul>
                   <ul
@@ -196,7 +196,7 @@ const Header = () => {
               </div>
             </div>
             {/* Responsive navbar */}
-            <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+            <Link className="navbar-burger self-center mr-12 xl:hidden" to="#">
               <button
                 type="button"
                 className="text-white inline-flex p-3 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
@@ -240,7 +240,7 @@ const Header = () => {
                   </ul>
                 </div>
               </button>
-            </a>
+            </Link>
           </nav>
         </section>
       </div>
