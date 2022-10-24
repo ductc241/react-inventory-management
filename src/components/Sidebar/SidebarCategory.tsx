071d01919fc8fd5c8186d6956ee7f3a862cde3bb
wizeKieu <kieunghiep@wize-net.co.jp>
2022-10-24 09:22:52 +0700

    remove dark sidebar

--- src/components/Sidebar/SidebarCategory.tsx
+++ src/components/Sidebar/SidebarCategory.tsx
@@ -6,18 +6,15 @@ const SidebarCategory = () => {
   return (
     <div>
       <aside className="w-auto border rounded" aria-label="Sidebar">
-        <div className="overflow-y-auto bg-white rounded dark:bg-slate-300 drop-shadow-xl">
+        <div className="overflow-y-auto bg-white rounded drop-shadow-xl">
           <ul className="space-y-2">
             <li>
               <div
-                className="flex items-center p-2 w-full text-lg font-normal text-gray-900 transition duration-75 group dark:text-white bg-[#F3F4F6]"
+                className="flex items-center p-2 w-full text-lg font-normal text-gray-900 transition duration-75 group  bg-[#F3F4F6]"
                 aria-controls="dropdown-example"
                 data-collapse-toggle="dropdown-example"
               >
-                <span
-                  className="flex-1 ml-3 text-left whitespace-nowrap"
-                  sidebar-toggle-item
-                >
+                <span className="flex-1 ml-3 text-left whitespace-nowrap">
                   Nhóm hàng
                 </span>
                 <div className="add_sidebar mr-2">
@@ -33,7 +30,7 @@ const SidebarCategory = () => {
             <li className="px-3">
               <Link
                 to="#"
-                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500"
+                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg  hover:bg-gray-100 "
               >
                 <span className="flex-1 whitespace-nowrap">Củ sạc</span>
               </Link>
@@ -41,7 +38,7 @@ const SidebarCategory = () => {
             <li className="px-3">
               <Link
                 to="#"
-                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500"
+                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100"
               >
                 <span className="flex-1 whitespace-nowrap">Tai nghe</span>
               </Link>
@@ -49,7 +46,7 @@ const SidebarCategory = () => {
             <li className="px-3">
               <Link
                 to="#"
-                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500"
+                className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg  hover:bg-gray-100"
               >
                 <span className="flex-1 whitespace-nowrap">Dây sạc</span>
               </Link>
--- src/components/Sidebar/SidebarThuocTinh.tsx
+++ src/components/Sidebar/SidebarThuocTinh.tsx
@@ -6,18 +6,15 @@ const SidebarThuocTinh = () => {
   return (
     <div>
       <aside className="w-auto border rounded" aria-label="Sidebar">
-        <div className="overflow-y-auto bg-white rounded dark:bg-slate-300 drop-shadow-xl">
+        <div className="overflow-y-auto bg-white rounded drop-shadow-xl">
           <ul className="space-y-2">
             <li>
               <div
-                className="flex items-center p-2 w-full text-lg font-normal text-gray-900 transition duration-75 group dark:text-white bg-[#F3F4F6]"
+                className="flex items-center p-2 w-full text-lg font-normal text-gray-900 transition duration-75 group"
                 aria-controls="dropdown-example"
                 data-collapse-toggle="dropdown-example"
               >
-                <span
-                  className="flex-1 ml-3 text-left whitespace-nowrap"
-                  sidebar-toggle-item
-                >
+                <span className="flex-1 ml-3 text-left whitespace-nowrap">
                   Thuộc Tính
                 </span>
                 <div className="add_sidebar mr-2">
@@ -31,35 +28,35 @@ const SidebarThuocTinh = () => {
               </div>
             </li>
             <li className="px-3">
-              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500">
+              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100">
                 <span className="flex-1 whitespace-nowrap">
                   <input type="text" placeholder="MÀU" />
                 </span>
               </div>
             </li>
             <li className="px-3">
-              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500">
+              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100">
                 <span className="flex-1 whitespace-nowrap">
                   <input type="text" placeholder="SỐ LƯỢNG" />
                 </span>
               </div>
             </li>
             <li className="px-3">
-              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500">
+              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100">
                 <span className="flex-1 whitespace-nowrap">
                   <input type="text" placeholder="KÍCH CỠ" />
                 </span>
               </div>
             </li>
             <li className="px-3">
-              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500">
+              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100">
                 <span className="flex-1 whitespace-nowrap">
                   <input type="text" placeholder="HÃNG" />
                 </span>
               </div>
             </li>
             <li className="px-3">
-              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-500">
+              <div className="flex items-center p-2 text-sm font-normal text-gray-900 rounded-lg hover:bg-gray-100">
                 <span className="flex-1 whitespace-nowrap">
                   <input type="text" placeholder="MỆNH GIÁ" />
                 </span>
