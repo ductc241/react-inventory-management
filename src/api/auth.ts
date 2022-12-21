95ee0b55dd7500e6414c254aa08ab78ce9d4d6ba
Quang Hieu <thienhotdog@gmail.com>
2022-12-21 09:10:37 +0700

    update

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,6 +2,7 @@ import { UserType } from "../types/user.type";
 import instance from "./instance";
 
 export const signin = (user: UserType) => {
+  // console.log(user);
   const url = "/login";
   return instance.post(url, user);
 };
