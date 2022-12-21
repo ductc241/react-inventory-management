1b4580bcf79994fa5ea91cbf30aae5c7a716b41d
Quang Hieu <thienhotdog@gmail.com>
2022-12-21 09:04:13 +0700

    update

--- src/store/slice/auth.slice.ts
+++ src/store/slice/auth.slice.ts
@@ -24,35 +24,6 @@ const authSlice = createSlice({
       state.user = null;
       localStorage.clear();
     }
-  },
-  extraReducers: (builder) => {
-    builder.addCase(signIn.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(signIn.fulfilled, (state, { payload }) => {
-      state.isLoading = false;
-      state.user = payload.user;
-      state.token = localStorage.setItem(
-        "token",
-        JSON.stringify(payload.accessToken)
-      );
-    });
-    builder.addCase(signIn.rejected, (state, action) => {
-      state.isLoading = true;
-      state.error = true;
-    });
-    builder.addCase(signUp.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(signUp.fulfilled, (state, { payload }) => {
-      state.isLoading = false;
-      toast.success("Đăng ký thành công!");
-    });
-    builder.addCase(signUp.rejected, (state, action) => {
-      state.isLoading = true;
-      state.error = true;
-      toast.error("Lỗi");
-    });
   }
   // eslint-disable-next-line @typescript-eslint/no-empty-function
 });
