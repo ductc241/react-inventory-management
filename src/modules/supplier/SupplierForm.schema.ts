bfe288af266cd60255a0f5526459ebdb3cf33263
Quang Hieu <thienhotdog@gmail.com>
2023-02-07 19:30:16 +0700

    update

--- src/modules/supplier/SupplierForm.schema.ts
+++ src/modules/supplier/SupplierForm.schema.ts
@@ -6,7 +6,7 @@ const SupplierSchema = yup
     address: yup.string().required("Đây là trường bắt buộc"),
     phone: yup
       .number()
-      .typeError("Đây là trường bắt buộc")
+      .typeError("Bạn chưa nhập đúng định dạng")
       .required("Đây là trường bắt buộc")
   })
   .required();
